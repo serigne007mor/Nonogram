906adb77ddb70da8d90323fc6a487786fdbf930b
serigne007mor <serignemortoure1@gmail.com>
2020-01-01 01:21:40 -0500

    changed sat variable data structure from array to List for better use

--- Nonogrm/src/app/Main.java
+++ Nonogrm/src/app/Main.java
@@ -1,8 +1,10 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,21 +16,26 @@ public class Main {
         Set<VarInteger> cspVarSet = new HashSet<VarInteger>(xcsp3Var);
         Set<Extension> extensionsSet = nonogram.getExtensionSet();
 
-        Map<VarInteger, SatVar[]> satTerm = new HashMap<VarInteger, SatVar[]>();
+        Map<VarInteger, List<SatVar>> satTerm = new HashMap<VarInteger, List<SatVar>>();
         int j = 0;
+        Map<Integer, Integer> hel = new HashMap<Integer, Integer>();
+        hel.put(1, 2);
+        hel.put(3, 4);
         for(VarInteger x: cspVarSet){
             int lenght = x.getValue().length;
             // System.out.println(x.id+" is:  "+lenght);
-            SatVar[] sat = new SatVar[lenght];
+            List<SatVar> sat = new ArrayList<>();
             for(int i = 0; i <lenght ;i++){
-            sat[i] = new SatVar("t"+j+""+i, false);
+            sat.add(i, new SatVar("t"+j+""+i, false));
 
             }
+            // System.out.print(sat.toString());
             satTerm.put(x, sat);
             j++;
         }
-        System.out.println(extensionsSet.toString());
-        // System.out.println(satTerm.toString());
+        // System.out.println(extensionsSet.toString());
+        System.out.println(satTerm.toString());
+        // System.out.println(hel.toString());
     }
 
 }
