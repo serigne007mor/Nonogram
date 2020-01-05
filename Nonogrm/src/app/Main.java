d9446a2531975bb7aa0e2fe1a49548728b7f179c
serigne007mor <serignemortoure1@gmail.com>
2020-01-05 14:50:57 -0500

    created al the sat variables, and print them out to a file in output folder

--- Nonogrm/src/app/Main.java
+++ Nonogrm/src/app/Main.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -11,31 +12,43 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws Exception {
         String path = "Nonogrm/instances/small/test-001-ternary.xml";
+        String outputPath = "Nonogrm/output/minisatFile";
+        String output = "";
+        String negation = "";
         XCSP3 nonogram = new XCSP3(path);
         Collection<VarInteger> xcsp3Var = nonogram.getMapVar();
         Set<VarInteger> cspVarSet = new HashSet<VarInteger>(xcsp3Var);
         Set<Extension> extensionsSet = nonogram.getExtensionSet();
 
         Map<VarInteger, List<String>> satTerm = new HashMap<VarInteger, List<String>>();
-        int j = 0;
         Map<Integer, Integer> hel = new HashMap<Integer, Integer>();
         hel.put(1, 2);
         hel.put(3, 4);
-        for(VarInteger x: cspVarSet){
+        int j = 0;
+        for (VarInteger x : cspVarSet) {
             int lenght = x.getValue().length;
-            // System.out.println(x.id+" is:  "+lenght);
             List<String> sat = new ArrayList<>();
-            for(int i = 0; i <lenght ;i++){
-            sat.add(i, "t"+j+""+i);
-
+            for (int i = 0; i < lenght; i++) {
+                sat.add(i, "t" + j + "" + i);
+                negation += "t" + j + "" + i + " -t" + j + "" + i + " 0\n";
+                output += "t" + j + "" + i + " ";
             }
-            // System.out.print(sat.toString());
+            output += "0\n";
             satTerm.put(x, sat);
             j++;
         }
-        // System.out.println(extensionsSet.toString());
-        System.out.println(satTerm.toString());
-        // System.out.println(hel.toString());
+        // System.out.println(satTerm.toString());
+        output+= negation;
+        System.out.println(output);
+        // System.out.println(negation);
+
+        // write to the file
+        PrintWriter satFile = new PrintWriter(outputPath, "UTF-8");
+        satFile.println("c this is nonogram instance");
+        satFile.println("p cnf 680");
+        satFile.print(output.trim());
+        satFile.close();
+
     }
 
 }
